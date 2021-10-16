b07c26a8b96b0f4867dbbe59123b1e91737b8573
Spasko Katsarski <patsokatsarski1@gmail.com>
2021-10-16 14:40:42 +0300

    Add project files.

---
+++ Building/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Building
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numOfFloors = int.Parse(Console.ReadLine());
+            int numOfRooms = int.Parse(Console.ReadLine());
+
+            for (int floor = numOfFloors; floor >= 1; floor--)
+            {
+                for (int room = 0; room < numOfRooms; room++)
+                {
+                    if (floor == numOfFloors)
+                    {
+                        Console.Write($"L{floor}{room} ");
+                        continue;
+                    }
+                    if (floor % 2 == 1)
+                    {
+                        Console.Write($"A{floor}{room} ");
+                    }
+                    else
+                    {
+                        Console.Write($"O{floor}{room} ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ SumOfTwoNumbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace SumOfTwoNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = int.Parse(Console.ReadLine());
+            int end = int.Parse(Console.ReadLine());
+            int magicNumber = int.Parse(Console.ReadLine());
+
+            int counter = 0;
+
+            for (int n1 = start; n1 <= end; n1++)
+            {
+                for (int n2 = start; n2 <= end; n2++)
+                {
+                    counter++;
+                    if (n1 + n2 == magicNumber)
+                    {
+                        Console.WriteLine($"Combination N:{counter} ({n1} + {n2} = {magicNumber})");
+                        return;
+                    }
+                }              
+            }  
+            Console.WriteLine($"{counter} combinations - neither equals {magicNumber}");
+        }
+    }
+}
